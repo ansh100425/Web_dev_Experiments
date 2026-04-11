5fbeec22e7a9520564601763fd0b9a187e29dd8b
anshumankhandka <anshumankhandka2006@gmail.com>
2026-04-11 10:29:27 +0530

    Delete brain.js

--- brain.js
+++
@@ -1,9 +0,0 @@
-//  this show a welcome message when page loads
-alert("Welcome to my Portfolio Website!");
-
-// header color change
-const header = document.querySelector("header");
-
-header.addEventListener("click", function () {
-    header.style.backgroundColor = "#0f172a";
-});
