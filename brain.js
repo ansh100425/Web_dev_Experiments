5584d61491ca11c363370ff27a21566aa4368687
anshumankhandka <anshumankhandka2006@gmail.com>
2026-04-11 10:23:44 +0530

    Add files via upload

---
+++ brain.js
@@ -0,0 +1,9 @@
+//  this show a welcome message when page loads
+alert("Welcome to my Portfolio Website!");
+
+// header color change
+const header = document.querySelector("header");
+
+header.addEventListener("click", function () {
+    header.style.backgroundColor = "#0f172a";
+});
